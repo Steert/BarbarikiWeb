e62f2cf5e53bb37244a9456b0333320542b5c031
ASdev-coder <ant.skliar777@gmail.com>
2026-02-25 17:50:00 +0200

    Create client, add client header etc

---
+++ Client/src/components/header/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="container">
+        <div className="info">
+          <div>
+            <img src="../src/assets/header-logo.png" alt="logo wellness kit image" className="logo" />
+          </div>
+          <div className="text-info">
+            <p className="text">Instant</p>
+            <p className="text">Wellness Kits</p>
+          </div>
+        </div>
+    </div>
+  );
+};
+
+export default Header;
