2bc197e932d42049bb0e07613716edc11084f55d
Danil Kuschenko <lagasifo@gmail.com>
2026-02-28 18:35:15 +0200

    refactory: docker compose

--- Server/WebApi/Program.cs
+++ Server/WebApi/Program.cs
@@ -4,13 +4,12 @@ using dotenv.net;
 
 var builder = WebApplication.CreateBuilder(args);
 
-Console.WriteLine(Environment.GetEnvironmentVariable("DATABASE_STRING_KEY"));
-
 DotEnv.Load(options: new DotEnvOptions(probeForEnv: true, probeLevelsToSearch: 4));
 
 builder.Services.AddDataAccess(builder.Configuration, Environment.GetEnvironmentVariable("DATABASE_STRING_KEY"));
 builder.Services.AddBusinessLogic();
 builder.Services.AddControllers();
+    
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("ReactCors", policy =>
@@ -22,11 +21,14 @@ builder.Services.AddCors(options =>
     });
 });
 builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.UseCors("ReactCors");
-app.UseCors("AllowReactApp");
-app.MapControllers();
 app.UseSwagger();
 app.UseSwaggerUI();
+
+app.UseCors("ReactCors");
+
+app.MapControllers();
+
 app.Run();
